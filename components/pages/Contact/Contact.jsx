ee6ec69d26dd531f194f06d583004d42010cbfdb
chrisnastro <cnastro@icloud.com>
2024-03-25 00:38:51 -0400

    removed unnecessary imports

--- components/pages/Contact/Contact.jsx
+++ components/pages/Contact/Contact.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import { TextField, dividerClasses } from "@mui/material";
-import { Button, MenuItem } from "@mui/material";
-import Stack from "@mui/material/Stack";
 import "./Contact.css";
 
 export default function Contact() {
